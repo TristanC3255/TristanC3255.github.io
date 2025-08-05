5db18a399ab92bfe38e28731de68ebc5b30ff7b8
Tristan Chong <tristanchong.3255@gmail.com>
2025-08-04 21:53:45 -0400

    Add portfolio source files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import './App.css';
+
 export default function App() {
   return (
     <div>
       <h1>Hello, I'm Tristan 👋</h1>
-      <p>This is my React portfolio site, deployed to GitHub Pages.</p>
+      <p>This is my React portfolio site, deployed to GitHub Pages! </p>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()} Tristan Chong</p>
+    </footer>
+  );
+}
+
